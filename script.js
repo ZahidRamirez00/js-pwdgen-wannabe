91ab4ecffc751dfa09c6094b9123f998ee6b9d01
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-08 18:02:59 +0100

    Prova delle variabile con la  console

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = prompt("Inserisci il tuo nome: ");
+let cognome = prompt("Inserisci il tuo cognome: ");
+let colore = prompt("Inserisci il tuo colore preferito: ");
+let numero1 = prompt("Inserisci il primo numero: ");
+let numero2 = prompt("Inserisci il secondo numero: ");
+let divisione = numero1 / numero2;
+
+console.log(nome);
+console.log(cognome);
+console.log(colore);
+console.log(numero1);
+console.log(numero2);
+console.log(divisione);

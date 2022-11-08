7cd8810332b9c5330d1819cdb36f6eb940d4ca10
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-08 18:27:48 +0100

    Fine esercizio

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ let numero2 = prompt("Inserisci il secondo numero: ");
 let divisione = numero1 / numero2;
 
 console.log(nome);
+document.getElementById("nome").value = nome;
 console.log(cognome);
+document.getElementById("cognome").value = cognome;
 console.log(colore);
+document.getElementById("colore").value = colore;
 console.log(numero1);
 console.log(numero2);
 console.log(divisione);
+
+let password = nome + cognome + colore + divisione;
+
+console.log(password);
+document.getElementById("password").value = password;
